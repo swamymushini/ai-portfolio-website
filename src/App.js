8f16cd903fb07c92d223a1c8e82b509f244fc646
swamymushini <gmushini@rocketsoftware.com>
2025-04-27 00:08:50 +0530

    added all resumes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
 import { HashRouter, Route, Routes } from "react-router-dom";
 
 // Component to handle redirection
 const ResumeRedirect = () => {
+  const location = useLocation();
+
   useEffect(() => {
-    window.location.href = "https://drive.google.com/file/d/1r9eRqU2kVOeJcZJHOhSgwDd9so_aWc2q/view";
-  }, []);
+    // Map routes to the correct resume URLs
+    const resumeMap = {
+      '/r25': 'https://gopala-swamy.netlify.app/Gopala_Swamy___Senior_Software_Engineer-25.pdf',
+      '/resume': 'https://gopala-swamy.netlify.app/Gopala_Swamy___Senior_Software_Engineer-25.pdf',
+      '/r25a': 'https://gopala-swamy.netlify.app/Gopala_Swamy___Senior_Software_Engineer-25-a.pdf',
+      '/r252': 'https://gopala-swamy.netlify.app/Gopala_Swamy___Senior_Software_Engineer-252.pdf',
+      '/r252a': 'https://gopala-swamy.netlify.app/Gopala_Swamy___Senior_Software_Engineer-252-a.pdf',
+      '/r25z': 'https://gopala-swamy.netlify.app/Gopala_Swamy___Senior_Software_Engineer-25z.pdf',
+      '/r25za': 'https://gopala-swamy.netlify.app/Gopala_Swamy___Senior_Software_Engineer-25z-a.pdf',
+    };
+
+    const path = location.pathname.toLowerCase();
+    const targetUrl = resumeMap[path];
+
+    if (targetUrl) {
+      window.location.href = targetUrl;
+    } else {
+      // Optional: fallback if path not found
+      window.location.href = 'https://gopala-swamy.netlify.app';
+    }
+  }, [location]);
 
   return null; // Nothing to render
 }
@@ -17,8 +39,15 @@ function App() {
     <div>
       <HashRouter>
         <Routes>
-          {/* Route for redirecting /resume */}
+          {/* Dynamic redirect for all /r* routes */}
+          <Route path="/r25" element={<ResumeRedirect />} />
           <Route path="/resume" element={<ResumeRedirect />} />
+          <Route path="/r25a" element={<ResumeRedirect />} />
+          <Route path="/r252" element={<ResumeRedirect />} />
+          <Route path="/r252a" element={<ResumeRedirect />} />
+          <Route path="/r25z" element={<ResumeRedirect />} />
+          <Route path="/r25za" element={<ResumeRedirect />} />
+          
           {/* BaseLayout to handle all other routes */}
           <Route path="/*" element={<BaseLayout />} />
         </Routes>
